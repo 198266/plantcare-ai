8f368a0987bfca4226c04ff2aded1fa99a64bd43
198266 <thomaslegrand57@outlook.fr>
2026-08-03 13:18:46 +0200

    Initialize PhytoSense app with home screen
    Connexion de l'écran d'accueil

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-// PhytoSense AI
-// Point d'entrée de l'application
+import 'package:flutter/material.dart';
+import 'features/home/home_screen.dart';
 
 void main() {
-  print("PhytoSense AI démarre 🌱");
+  runApp(const PhytoSenseApp());
+}
+
+class PhytoSenseApp extends StatelessWidget {
+  const PhytoSenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PhytoSense AI',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
